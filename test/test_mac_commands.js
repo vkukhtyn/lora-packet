aeee00b906bce1bdac9dd20699d4a19d1038ee69
Viacheslav Kukhtyn <v.kukhtyn@gmail.com>
2020-09-18 22:17:59 +0300

    Add tests for DeviceStatusAns constructor

--- test/test_mac_commands.js
+++ test/test_mac_commands.js
@@ -1,19 +1,19 @@
 'use strict';
 
-var lora_packet = require('..');
-var expect = require("chai").expect;
-var assert = require('chai').assert;
+let lora_packet = require('../lib/index.js');
+let expect = require("chai").expect;
+let assert = require('chai').assert;
 
 module.exports = function () {
     describe('parse MAC commands in downlink', function () {
-        var downlinkMacCommands = lora_packet.downlinkMacCommands();
+        let downlinkMacCommands = lora_packet.downlinkMacCommands();
 
         it('should parse LinkCheckAns', function () {
 
         });
         it('should parse DevStatusReq', function () {
-            var message_hex = '60A5F59B0F810B0006505154DC';
-            var packet = lora_packet.fromWire(new Buffer(message_hex, 'hex'));
+            let message_hex = '60A5F59B0F810B0006505154DC';
+            let packet = lora_packet.fromWire(new Buffer(message_hex, 'hex'));
             expect(packet).to.not.be.undefined;
             expect(packet.getDir()).to.equal('down');
             expect(packet.getBuffers().FOpts[0]).to.deep.equal(downlinkMacCommands.DEV_STATUS_REQ.cid);
@@ -22,18 +22,28 @@ module.exports = function () {
     });
 
     describe('parse MAC commands in uplink', function () {
-        var uplinkMacCommands = lora_packet.uplinkMacCommands();
+        let uplinkMacCommands = lora_packet.uplinkMacCommands();
 
         it('should parse LinkCheckReq', function () {
 
         });
         it('should parse DevStatusAns', function () {
-            var message_hex = "40A5F59B0F83DE0106FE0E29F3513B";
-            var packet = lora_packet.fromWire(new Buffer(message_hex, 'hex'));
+            let message_hex = "40A5F59B0F83DE0106FE0E29F3513B";
+            let packet = lora_packet.fromWire(new Buffer(message_hex, 'hex'));
             expect(packet).to.not.be.undefined;
             expect(packet.getDir()).to.equal('up');
             expect(packet.getBuffers().FOpts[0]).to.deep.equal(uplinkMacCommands.DEV_STATUS_ANS.cid);
             expect(packet.getBuffers().FOpts.length).to.equal(uplinkMacCommands.DEV_STATUS_ANS.size + 1);
         });
+    });
+
+    describe('create MAC commands from byte array', function() {
+        it('fail on DeviceStatusReq creation', function() {
+
+        })
+        it('fail on DeviceStatusAns creation', function() {
+            expect(() => new lora_packet.DeviceStatusAns(new Buffer('00', 'hex'))).to.throw(Error, 'DeviceStatusAns requires CID [6] but [0] received');
+            expect(() => new lora_packet.DeviceStatusAns(new Buffer('06010203', 'hex'))).to.throw(Error, 'DeviceStatusAns size should be [2]');
+        })
     });
 };
